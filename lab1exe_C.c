c83017c419e2669da0b408fd357ee9288147bb0a
GraydonHall42 <gwhall@ualberta.ca>
2021-09-18 15:51:53 -0600

    GH complete part C. Confirmed same result as Jared

--- lab1exe_C.c
+++ lab1exe_C.c
@@ -15,33 +15,33 @@ int main(void)
 
   bar = 100;
   foo = 200;
-  fred = &foo;
+  fred = &foo;  // fred is pointer for foo
 
   /* point one */
 
   printf("point one: foo is %d and bar is %d.\n", foo, bar);
-  sam = &bar;
-  *sam += 30;
-  *fred -= 40;
+  sam = &bar;  // sam is pointer for bar
+  *sam += 30;  // bar increased by 30
+  *fred -= 40;  // fred decreased by 40
 
   /* point two */
 
   printf("point two: foo is %d and bar is %d.\n", foo, bar);
-  fred = &bar;
-  *fred += 5;
+  fred = &bar;  // fred is now pointer for bar
+  *fred += 5;  // bar is increased by 5
 
   /* point three */
 
   printf("point three: foo is %d and bar is %d.\n", foo, bar);
   printf("point three: *fred is %d and *sam is %d.\n", *fred, *sam);
-  sam = &foo;
-  *sam = *fred;
+  sam = &foo;  // sam is now pointer for foo
+  *sam = *fred;  // foo becomes equal to bar
 
   /* point four */
 
   printf("point four: foo is %d and bar is %d.\n", foo, bar);
-  *sam *= 100;
-  sam = fred;
+  *sam *= 100;  // foo multiplied by 100
+  sam = fred;  //  sam now same as fred
 
   /* point five */
 
